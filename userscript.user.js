bda158c684dcc768d75f660ac37de8a01521fffe
Tony Wu <tonywu.ca@gmail.com>
2022-04-02 17:14:08 -0400

    reverted link change for now

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/sssata/canada_place_template/main/template.png";
+            i.src = "https://github.com/sssata/canada_place_template/blob/main/template.png?raw=true";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
