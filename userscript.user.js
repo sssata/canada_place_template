b72bbf51da3ba36ad4f54f55a40fb5170096c667
Tony Wu <tonywu.ca@gmail.com>
2022-04-02 00:33:52 -0400

    added tampermonkey userscript

---
+++ userscript.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         r/place Canada Template
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Spread the love
+// @author       oralekin
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://github.com/sssata/canada_place_template/blob/main/template.png?raw=true";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
